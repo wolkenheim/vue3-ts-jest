7eb24525976bc61c99fb48a049549ecd993048e3
shopfreelancer <shopfreelancer@gmail.com>
2020-11-09 10:05:54 +0100

    added PostWriter component

--- src/NewPost.vue
+++ src/NewPost.vue
@@ -1,10 +1,31 @@
 <template>
   <div>New Post</div>
+  <post-writer :post="post" />
 </template>
 
 <script lang="ts">
+import moment from "moment";
 import { defineComponent } from "vue";
+import PostWriter from "./PostWriter.vue";
+import { Post } from "./types";
 export default defineComponent({
   name: "NewPost",
+  components: {
+    PostWriter,
+  },
+  setup() {
+    const post: Post = {
+      id: -1,
+      title: "yep",
+      authorId: 1,
+      created: moment(),
+      markdown: "## New Post\nEnter your post here",
+      html: "",
+    };
+
+    return {
+      post,
+    };
+  },
 });
 </script>
---
+++ src/PostWriter.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div class="columns">
+      <div class="column">
+        <div class="field">
+          <div class="label">Post</div>
+          <div class="control">
+            <input v-model="title" type="text" class="input" />
+            {{ title }}
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="columns">
+      <div class="column is-one-half">
+        <div
+          contenteditable
+          id="markdown"
+          ref="contentEditable"
+          @input="handleEdit"
+        />
+      </div>
+      <div class="column is-one-half">
+        {{ markdown }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from "vue";
+import { Post } from "./types";
+export default defineComponent({
+  name: "PostWriter",
+  props: {
+    post: {
+      type: Object as () => Post,
+      required: true,
+    },
+  },
+  setup(props) {
+    const title = ref(props.post.title);
+    const contentEditable = ref<null | HTMLDivElement>(null);
+    const markdown = ref(props.post.markdown);
+
+    const handleEdit = () => {
+      markdown.value = contentEditable.value.innerText;
+    };
+
+    onMounted(() => {
+      contentEditable.value.innerText = markdown.value;
+    });
+    return {
+      title,
+      contentEditable,
+      handleEdit,
+      markdown,
+    };
+  },
+});
+</script>
