269741481d0b2fb4afd1e452cfd78fb38c3aff36
shopfreelancer <shopfreelancer@gmail.com>
2020-11-20 11:27:51 +0100

    added custom input component

---
+++ src/FormInput.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="field">
+    <label class="name" :for="name">{{ name }}</label>
+    <div class="control">
+      <input
+        class="input"
+        :id="name"
+        @input="handleInput"
+        :value="modelValue"
+      />
+    </div>
+    <p class="help is-danger" v-if="error">{{ error }}</p>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  props: {
+    error: {
+      type: String,
+    },
+    type: {
+      type: String,
+      default: "text",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    modelValue: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props, ctx) {
+    const handleInput = (event: any) => {
+      ctx.emit("update:modelValue", event.target.value);
+    };
+
+    return { handleInput };
+  },
+});
+</script>
